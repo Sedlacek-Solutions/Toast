9737da5466a94edfd15b75f2a47c8e97388a7902
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-01-19 18:45:46 -0500

    added trailing action button

--- Sources/Toast/Views/ToastMessageView.swift
+++ Sources/Toast/Views/ToastMessageView.swift
@@ -6,22 +6,37 @@
 
 import SwiftUI
 
-struct ToastMessageView: View {
+struct ToastMessageView<T: View>: View {
     private let toast: Toast
+	private let trailingButtonView: T?
 
-    init(_ toast: Toast) {
-        self.toast = toast
-    }
+	init(_ toast: Toast, @ViewBuilder trailingButtonView: () -> T? = { nil }) {
+		self.toast = toast
+		self.trailingButtonView = trailingButtonView()
+	}
 
     var body: some View {
         HStack(spacing: 10) {
             toast.icon
                 .font(.system(size: 24, weight: .medium))
                 .foregroundStyle(toast.color)
+
             Text(toast.message)
                 .font(.system(size: 18, weight: .semibold))
                 .foregroundStyle(.primary)
+
             Spacer(minLength: .zero)
+
+			if let trailingButtonView {
+				trailingButtonView
+					.padding(.horizontal, 10)
+					.padding(.vertical, 5)
+					.fontWeight(.semibold)
+					.foregroundStyle(toast.color)
+					.background(toast.color.opacity(0.2),
+								in: .rect(cornerRadius: 5)
+					)
+			}
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
@@ -37,7 +52,7 @@ struct ToastMessageView: View {
     }
 }
 
-extension ToastMessageView {
+extension ToastMessageView where T == EmptyView {
     static var infoExample: some View {
         ToastMessageView(.info(message: "Something informational for the user."))
     }
@@ -54,13 +69,19 @@ extension ToastMessageView {
         ToastMessageView(.success(message: "Successfully did the thing!"))
     }
 
-    static var noticeExample: some View {
-        ToastMessageView(.notice(message: "A software update is available."))
-    }
-
     static var debugExample: some View {
-        ToastMessageView(.debug(message: "Line 32 in `File.swift` executed."))
+		ToastMessageView(.debug(message: "Line 32 in `File.swift` executed."))
     }
+}
+
+extension ToastMessageView where T == Button<Text> {
+	static var noticeExample: some View {
+		ToastMessageView(.notice(message: "A software update is available.")) {
+			Button("Update") {
+				print("Update pressed")
+			}
+		}
+	}
 }
 
 #Preview {
@@ -69,7 +90,7 @@ extension ToastMessageView {
         ToastMessageView.warningExample
         ToastMessageView.errorExample
         ToastMessageView.successExample
-        ToastMessageView.noticeExample
         ToastMessageView.debugExample
+		ToastMessageView.noticeExample
     }
 }
